2ed293caf901331ac01717d07008aac5077e7e4a
Stef\Paweł <paw.stefanowski@gmail.com>
2019-05-22 15:22:16 +0200

    adding variables to the query changes

--- js/main.js
+++ js/main.js
@@ -1,101 +1,113 @@
 const tablabel =[];
 const tabdatas =[];
 //adding currency exchange rate after choosing currencies
+const fun = 'FX_INTERADAY';
+const interva = '15min';
+
+
+
+
 $( ".currencies-dropdown" ).change(function(){
   const f = document.getElementById("From");
   const from = f.options[f.selectedIndex].text;
   const t = document.getElementById("to");
   const to = t.options[t.selectedIndex].text;
-  document.getElementById("FromTo").innerHTML = "";
-  $('#FromTo').append('<p>'+from+' <=> '+to+'</p>');
+  if(from == '--Select--' || to == '--Select--') {
+    return;
+  }
+    document.getElementById("FromTo").innerHTML = "";
 
-  console.log(from);
-  console.log(to);
+    $('#FromTo').append('<p>' + from + ' <=> ' + to + '</p>');
 
-  const url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from + '&to_currency=' + to + '&apikey=3FTF5MNQVTS485JG';
-  console.log(url);
-  $.getJSON(url, function (data) {
+    console.log(from);
+    console.log(to);
 
-    //console.log(data['Realtime Currency Exchange Rate']['5. Exchange Rate']);
-    const wart = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
-    document.getElementById("value").innerHTML = "";
-    $('#value').append('<p>'+wart+'</p>');
-  });
+    const url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from + '&to_currency=' + to + '&apikey=3FTF5MNQVTS485JG';
+    console.log(url);
+    $.getJSON(url, function (data) {
 
-
-const urlchart ='https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=' + from + '&to_symbol=' + to + '&apikey=3FTF5MNQVTS485JG'
-  $.getJSON(urlchart, function (data) {
-
-    tabdatas.length=0;
-    tablabel.length=0;
-    $.each(data["Time Series FX (Daily)"], function (key, value) {
-      tablabel.push(key);
-      tabdatas.push(value["1. open"]);
-      console.log(value["1. open"]);
-      console.log(key);
-
+      //console.log(data['Realtime Currency Exchange Rate']['5. Exchange Rate']);
+      const wart = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+      document.getElementById("value").innerHTML = "";
+      $('#value').append('<p>' + wart + '</p>');
     });
-    tabdatas.reverse();
-    tablabel.reverse();
-
-  })
 
 
-  //const CHART = function(){
-  //  $('#lineChart').remove(); // this is my <canvas> element
-   // $('#chart-container').append('<canvas id="lineChart" width="500" height="300"><canvas>');
+    const urlchart = 'https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=' + from + '&to_symbol=' + to + '&apikey=3FTF5MNQVTS485JG'
+ // https://www.alphavantage.co/query?function=FX_INTRADAY&from_symbol=EUR&to_symbol=USD&interval=5min&apikey=demo
+    $.getJSON(urlchart, function (data) {
 
-  //};
+      tabdatas.length = 0;
+      tablabel.length = 0;
+      $.each(data["Time Series FX (Daily)"], function (key, value) {
+        tablabel.push(key);
+        tabdatas.push(value["1. open"]);
+        console.log(value["1. open"]);
+        console.log(key);
 
+      });
+      tabdatas.reverse();
+      tablabel.reverse();
 
-  let lineChart = new Chart(CHART, {
-
-    type: 'line',
-    data: {
-
-      labels: tablabel,
-
-      datasets: [
-        {
-
-          label: "Historical exchange chart",
-          //linia
-          //borderDash: [3, 3], //jezeli ustawione to przerywana linia
-          borderColor: 'rgba(8,72,134,0.5)',
-          pointBorderColor: 'rgba(8,72,134,0.5)',
-          borderWidth: 2,
-          lineTension: 0.1,
-          //kolor tla i legendy
-          fill: true, //czy wypelnic zbior
-          backgroundColor: 'rgba(114,207,220,0.1)', //wplywa tez na kolor w legendzie
-          //ustawienia punktu
-          pointRadius: 1,
-          pointBorderWidth: 1,
-          pointBackgroundColor: 'rgba(255,255,255,1)',
-          //ustawienia punktu hover
-          pointHoverRadius: 1,
-          pointHoverBorderWidth: 5,
-          pointHoverBackgroundColor: 'rgba(255,255,255,1)',
-          pointHoverBorderColor: 'rgba(236,115,87, 1)',
+    })
 
 
-          data: tabdatas,
-        }
-      ]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            // beginAtZero: true
+    //const CHART = function(){
+    //  $('#lineChart').remove(); // this is my <canvas> element
+    // $('#chart-container').append('<canvas id="lineChart" width="500" height="300"><canvas>');
+
+    //};
+
+
+    let lineChart = new Chart(CHART, {
+
+      type: 'line',
+      data: {
+
+        labels: tablabel,
+
+        datasets: [
+          {
+
+            label: "Historical exchange chart",
+            //linia
+            //borderDash: [3, 3], //jezeli ustawione to przerywana linia
+            borderColor: 'rgba(8,72,134,0.5)',
+            pointBorderColor: 'rgba(8,72,134,0.5)',
+            borderWidth: 2,
+            lineTension: 0.1,
+            //kolor tla i legendy
+            fill: true, //czy wypelnic zbior
+            backgroundColor: 'rgba(114,207,220,0.1)', //wplywa tez na kolor w legendzie
+            //ustawienia punktu
+            pointRadius: 1,
+            pointBorderWidth: 1,
+            pointBackgroundColor: 'rgba(255,255,255,1)',
+            //ustawienia punktu hover
+            pointHoverRadius: 1,
+            pointHoverBorderWidth: 5,
+            pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+            pointHoverBorderColor: 'rgba(236,115,87, 1)',
+
+
+            data: tabdatas,
           }
-        }]
+        ]
       },
-      legend: {
-        position: "bottom"
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              // beginAtZero: true
+            }
+          }]
+        },
+        legend: {
+          position: "bottom"
+        }
       }
-    }
-  });
+    });
+
 })
 
 
