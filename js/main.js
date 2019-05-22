b397e65caf8f28f43a5f60c4ff4bbf5817a9841b
Stef\Paweł <paw.stefanowski@gmail.com>
2019-05-22 11:49:26 +0200

    adding datas from api to chart

--- js/main.js
+++ js/main.js
@@ -1,55 +1,106 @@
-const CHART = document.getElementById("lineChart");
-console.log(CHART);
+const tablabel =[];
+const tabdatas =[];
+//adding currency exchange rate after choosing currencies
+$( ".currencies-dropdown" ).change(function(){
+  const f = document.getElementById("From");
+  const from = f.options[f.selectedIndex].text;
+  const t = document.getElementById("to");
+  const to = t.options[t.selectedIndex].text;
+  document.getElementById("FromTo").innerHTML = "";
+  $('#FromTo').append('<p>'+from+' <=> '+to+'</p>');
 
-let lineChart = new Chart(CHART, {
+  console.log(from);
+  console.log(to);
 
-  type: 'line',
-  data: {
+  const url = 'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from + '&to_currency=' + to + '&apikey=J3IPR8A064WO3OI9';
+  console.log(url);
+  $.getJSON(url, function (data) {
 
-    labels: [ "2019-05-22", "2019-05-21", "2019-05-20", "2019-05-19", "2019-05-17", "2019-05-16", "2019-05-15","2019-05-14"],
-
-    datasets: [
-      {
-
-        label: "Historical exchange chart",
-        //linia
-        //borderDash: [3, 3], //jezeli ustawione to przerywana linia
-        borderColor: 'rgba(8,72,134,0.5)',
-        pointBorderColor: 'rgba(8,72,134,0.5)',
-        borderWidth: 2,
-        lineTension: 0.1,
-        //kolor tla i legendy
-        fill: true, //czy wypelnic zbior
-        backgroundColor: 'rgba(114,207,220,0.1)', //wplywa tez na kolor w legendzie
-        //ustawienia punktu
-        pointRadius: 1,
-        pointBorderWidth: 1,
-        pointBackgroundColor: 'rgba(255,255,255,1)',
-        //ustawienia punktu hover
-        pointHoverRadius: 1,
-        pointHoverBorderWidth: 5,
-        pointHoverBackgroundColor: 'rgba(255,255,255,1)',
-        pointHoverBorderColor: 'rgba(236,115,87, 1)',
+    console.log(data['Realtime Currency Exchange Rate']['5. Exchange Rate']);
+    const wart = data['Realtime Currency Exchange Rate']['5. Exchange Rate'];
+    document.getElementById("value").innerHTML = "";
+    $('#value').append('<p>'+wart+'</p>');
+  });
 
 
-        data: [4.57, 4.15, 3.87, 3.74, 4.06, 4.09, 4.28, 4.12],
-      }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          // beginAtZero: true
+const urlchart ='https://www.alphavantage.co/query?function=FX_DAILY&from_symbol=EUR&to_symbol=USD&apikey=J3IPR8A064WO3OI9'
+  $.getJSON(urlchart, function (data) {
+    $.each(data["Time Series FX (Daily)"], function (key, value) {
+      tablabel.push(key);
+      tabdatas.push(value["1. open"]);
+      console.log(value["1. open"]);
+      console.log(key);
+
+    });
+    tabdatas.reverse();
+    tablabel.reverse();
+
+  })
+  const CHART = function(){
+    $('#lineChart').remove(); // this is my <canvas> element
+    $('#chart-container').append('<canvas id="lineChart" width="500" height="300"><canvas>');
+
+  };
+
+
+  let lineChart = new Chart(CHART, {
+
+    type: 'line',
+    data: {
+
+      labels: tablabel,
+
+      datasets: [
+        {
+
+          label: "Historical exchange chart",
+          //linia
+          //borderDash: [3, 3], //jezeli ustawione to przerywana linia
+          borderColor: 'rgba(8,72,134,0.5)',
+          pointBorderColor: 'rgba(8,72,134,0.5)',
+          borderWidth: 2,
+          lineTension: 0.1,
+          //kolor tla i legendy
+          fill: true, //czy wypelnic zbior
+          backgroundColor: 'rgba(114,207,220,0.1)', //wplywa tez na kolor w legendzie
+          //ustawienia punktu
+          pointRadius: 1,
+          pointBorderWidth: 1,
+          pointBackgroundColor: 'rgba(255,255,255,1)',
+          //ustawienia punktu hover
+          pointHoverRadius: 1,
+          pointHoverBorderWidth: 5,
+          pointHoverBackgroundColor: 'rgba(255,255,255,1)',
+          pointHoverBorderColor: 'rgba(236,115,87, 1)',
+
+
+          data: tabdatas,
         }
-      }]
+      ]
     },
-    legend: {
-      position: "bottom"
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            // beginAtZero: true
+          }
+        }]
+      },
+      legend: {
+        position: "bottom"
+      }
     }
-  }
-});
+  });
+})
 
+
+//chart element
+//const CHART = document.getElementById("lineChart");
+
+
+
+
+//adding option in selectors
 let dropdown = $('.currencies-dropdown');
 dropdown.empty();
 dropdown.append('<option selected="true" disabled>--Select--</option>');
@@ -65,35 +116,4 @@ $.getJSON(url, function (data) {
 
 
 
-
-/*
-let currency = $('.currencyExchange');
-currency.empty();
-currency.append('<p>--0.00--</p>');
-const f = document.getElementById("From");
-const from = f.options[f.selectedIndex].text;
-const t = document.getElementById("to");
-const to = t.options[t.selectedIndex].text;
-*/
-/*
-$.getJSON('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=USD&to_currency=PLN&apikey=J3IPR8A064WO3OI9', function (data) {
-
-  console.log(data);
-
-});
-*/
-
-/*
-
-$.getJSON('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=' + from + '&to_currency=' + to + '&apikey=demo', function (data) {
-
-  console.log(data);
-
-  const json = JSON.parse(data);
-  const jsonex = alter(json["5. Exchange Rate"])
-  currency.append('<p>'+jsonex+'</p>')
-
-});
-
-*/
 
