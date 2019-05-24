dca007c233fddbe89af1def93f49c797287fe1e1
Stef\Paweł <paw.stefanowski@gmail.com>
2019-05-24 14:10:57 +0200

    creating additional tests

--- test.js
+++ test.js
@@ -1,22 +1,25 @@
-const operations = require('./main.js')
+//const Main = require('./js/main.js')
 const assert = require('chai').assert
 
-it('should return true', () => {
-  assert.equal(true, true)
-})
-
-
-
-/*
-describe('operators', () => {
-  describe('#realTimeExcangeRate', () =>{
-    it('should returns true', () =>{
-      //assert.equal(new operations.currencyUriProvider('test1', 'test1') ,'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=test1&to_currency=test1&apikey=J3IPR8A064WO3OI9')
-      assert.equal(true,true)
+describe('Component', () => {
+  describe('#method', () => {
+    it('should returns true', () => {
+      assert.isTrue(true)
     })
   })
 })
-*/
+
+
+
+describe('Main', () => {
+  describe('#currencyUriProvider', () =>{
+    it('should returns url', () =>{
+      assert.equal(new operations.currencyUriProvider('test1', 'test1') ,'https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=test1&to_currency=test1&apikey=J3IPR8A064WO3OI9')
+
+    })
+  })
+})
+
 
 
 /*
